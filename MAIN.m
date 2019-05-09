8edc62daa012dcd44531e251c07a540ff0e1b3ae
erik <erikgoron@hotmail.com>
2019-05-09 12:00:51 +0100

    revtra

--- MAIN.m
+++ MAIN.m
@@ -1,12 +1,13 @@
 clear all
-global q  Jac tstart tstep tend time qd qdd q0 q00 body0 Flag 
+global q  Jac tstart tstep tend time qd qdd q0  
 
 %comment to suppress warning about "variable is changing size every
 %iteration
 %#ok<*SAGROW>
 
-%Filename='rameurrevtrans.rtf';
-Filename='strandbeest_v4.rtf';
+
+% Filename='rameurrevtrans.rtf';
+% Filename='strandbeest_v4.rtf';
 
 
 
