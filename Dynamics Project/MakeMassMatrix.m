0a3a50f9cf706f380d05945151c203468d248156
erik <erikgoron@hotmail.com>
2019-05-22 08:35:12 +0100

    mass

--- Dynamics Project/MakeMassMatrix.m
+++ Dynamics Project/MakeMassMatrix.m
@@ -7,8 +7,8 @@ for i=1:Nbody
     i1=3*i-2;
     i2=i1+1;
     i3=i2+1;
-    M(i1,i1)=body.mass;
-    M(i2,i2)=body.mass;
-    M(i3,i3)=body.J;
+    M(i1,i1)=body(i).mass;
+    M(i2,i2)=body(i).mass;
+    M(i3,i3)=body(i).J;
 end
 end
