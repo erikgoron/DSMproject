592f9ce37dc3d8270cbff236f1847ffdc54cb008
erik <erikgoron@hotmail.com>
2019-05-22 10:41:27 +0100

    d

--- Dynamics Project/MAINDYN.m
+++ Dynamics Project/MAINDYN.m
@@ -1,16 +1,9 @@
 clear all
 global tspan body Nbody
 
-%comment to suppress warning about "variable is changing size every
-%iteration
-%#ok<*SAGROW>
-
-
 Filename='4bardyn.rtf';
-% Filename='strandbeest_v4.rtf';
 
 Readinputdata
-
 
 % Vector with initial conditions y0
 y0=q2y(body,Nbody);
