131669eb5869313279a705be7b8ca622359cc37e
erik <erikgoron@hotmail.com>
2019-05-23 12:54:10 +0100

    m

--- Dynamics Project/ForceContact.m
+++ Dynamics Project/ForceContact.m
@@ -9,16 +9,4 @@ j=Contactforce(k).ct;
 
 
 
-R=[0 -1;1 0];
-ni=[0;-1];
-nj=[0;1];
-ti=R*ni;
 
-
-dd=body(i).rd+body(i).B*Contactforce(k).spi*body(i).thetad-...
-    body(j).rd+body(j).B*Contactforce(k).spj*body(j).thetad;
-ddn=dot(dd,ni);
-
-vt=dd-ddn*ni;
-
-ffri=-Contactforce(k).c*fn*vt/(norm(vt));
