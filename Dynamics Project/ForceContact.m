e42414717fa85110664b83895fd92572e31bb836
David Metzger <dave.m@solnet.ch>
2019-05-23 12:37:17 +0100

    force contact 1

--- Dynamics Project/ForceContact.m
+++ Dynamics Project/ForceContact.m
@@ -1,1 +1,26 @@
-function [g]= ForceContact(g,body,contact)
+function [g]= ForceContact(g,body,ContactGround,Ncontactground)
+i=1;
+spPi=[1,0]';
+groundY=0;
+k=1000;
+c=50;
+
+B=[0 -1;1 0];
+
+cPi = body(i).r+body(i).A*spPi;
+cPid= body(i).rd+ body(i).B*spPi*body(i).thetad;
+
+d=groundY-cPi(2);
+dd=cPid(2);
+ddx=cPid(1);
+
+if d>0
+    f=[-c*ddx ;k*d-c*dd];
+   [g] = ApplyForce(g,f,spPi,body,i);
+  
+else
+    return
+end
+
+
+end
